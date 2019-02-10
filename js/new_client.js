3f394356961d3c862f17da5a7963fc530f82c250
AndreeaSauchea <andreeas96@gmail.com>
2019-02-10 17:57:47 +0200

    finished room

---
+++ js/new_client.js
@@ -0,0 +1,51 @@
+var API_URL = {
+    CREATE: 'http://localhost:8080//clients'
+};
+
+window.NewClient = {
+
+    load: function(){
+        $(".add-form").submit(function() {
+            const client = {
+                forename: $('input[name=forename]').val(),
+                name: $('input[name=name]').val(),
+                birthday: $('input[name=birthday]').val(),
+                cnp: $('input[name=cnp]').val(),
+                typeID: $('input[name=typeID]').val(),
+                seriesID: $('input[name=seriesID]').val(),
+                numberID: $('input[name=numberID]').val(),
+                street: $('input[name=street]').val(),
+                streetNumber: $('input[name=streetNumber]').val()
+            };
+
+                NewClient.save(client);
+
+        });
+    },
+
+    save: function(client) {
+        console.log(client);
+        $.ajax({
+            url: API_URL.CREATE,
+            headers: {
+
+                "Content-Type": "application/json"
+            },
+            method: "POST",
+            data: JSON.stringify(client, null, 2),
+            success: function() {
+                console.info ('We made it'),
+                alert('New client was created');
+                location.reload(true);
+              }
+        }).done(function (response) {
+            if (response.success) {
+                console.info('person', response);
+            }
+        });
+    },
+
+
+}
+console.info('loading persons');
+NewClient.load();
--- js/room.js
+++ js/room.js
@@ -2,7 +2,8 @@ var API_URL = {
     READ: 'http://localhost:8080/rooms/',
     READBOOKED: 'http://localhost:8080/bookedrooms/room/',
     ADD: 'http://localhost:8080/bookedroom/{roomId}/activity/{activityId}/add',
-    REMOVE: 'http://localhost:8080/bookedroom/{roomId}/activity/{activityId}/remove'
+    REMOVE: 'http://localhost:8080/bookedroom/{roomId}/activity/{activityId}/remove',
+    GETSERVICES: 'http://localhost:8080/services'
 };
 
 
@@ -33,17 +34,27 @@ window.Room = {
         const urlParams = new URLSearchParams(window.location.search);
         const id = urlParams.get('id');
         console.info('roomId works', id)
+        $.ajaxSetup({
+            error: function(xhr){
+                alert('Request Status: ' + xhr.status + ' Status Text: Room is not booked' );
+                var x = document.getElementById("booked");
+                x.style.display = "none";
+            }
+        });
         $.ajax({
             url: API_URL.READBOOKED + id,
-            method: "GET"
+            method: "GET",
         }).done(function (bookedroom) {
             console.info('done: bookedroom', bookedroom);
-            
             var x = document.getElementById("booked");
             if(bookedroom.client){
+                console.info('booked', bookedroom.client)
                 x.style.display = "block";
+                client = bookedroom.client;
+                totalPrice = bookedroom.totalPrice;
                 Room.displayBooked(bookedroom)
-                Room.displayActivities(bookedroom.serviceList);
+                Room.loadService(bookedroom);
+                Room.bindEvents(bookedroom);
             } else{
                 x.style.display = "none";
             }
@@ -66,18 +77,11 @@ window.Room = {
         </tr>`;
     },
 
-    getActionRow: function() {
-        return '<tr>' +
-            '<td><input type="text" name="serviceName" placeholder="Activity name"></td>' +
-            '<td><input type="number" required name="servicePrice" placeholder="Price"></td>' +
-            '<td><button type="submit">Save</button></td>' +
-            '</tr>';
-    },
-
     delete: function(activityId) {
         const urlParams = new URLSearchParams(window.location.search);
         const roomId = urlParams.get('id');
-        console.info('roomId works', id)
+        console.info('roomId works', roomId);
+        console.info('activityId works', activityId);
         $.ajax({
             url: API_URL.REMOVE.replace("{roomId}", roomId).replace("{activityId}", activityId),
             method: "PUT"
@@ -85,31 +89,30 @@ window.Room = {
         });
     },
 
-    add: function(person) {
+    add: function() {
+        console.info('add ', newActivityId);
         const urlParams = new URLSearchParams(window.location.search);
         const roomId = urlParams.get('id');
-        console.info('roomId works', id)
+        console.info('roomId works', roomId);
+        console.info('newActivityId works', newActivityId);
         $.ajax({
-            url: API_URL.ADD.replace("{roomId}", roomId).replace("{activityId}", activityId),
+            url: API_URL.ADD.replace("{roomId}", roomId).replace("{activityId}", newActivityId),
             method: "PUT"
         }).done(function () { 
         });
     },
 
 
-    bindEvents: function() {
-        $('#phone-book tbody').delegate('a.edit', 'click', function () {
-            var id = $(this).data('id');
-            PhoneBook.edit(id);
+    bindEvents: function(bookedroom) {
+
+        $('#table-overrides tbody').delegate('a.delete', 'click', function () {
+            var activityId = $(this).data('id');
+            console.info('click on ', this, activityId);
+            Room.delete(activityId);
         });
 
-        $('#phone-book tbody').delegate('a.delete', 'click', function () {
-            var id = $(this).data('id');
-            console.info('click on ', this, id);
-            PhoneBook.delete(id);
-        });
 
-        $( ".add-form" ).submit(function() {
+       /* $( ".add-form" ).submit(function() {
             const person = {
                 firstName: $('input[name=firstName]').val(),
                 lastName: $('input[name=lastName]').val(),
@@ -122,21 +125,106 @@ window.Room = {
             } else {
                 PhoneBook.add(person);
             }
-        });
+        });*/
     },
 
     
-    displayActivities: function(serviceList) {
+    displayActivities: function(existingServiceList, newServiceList) {
+        newServices = newServiceList;
         var rows = '';
 
-        serviceList.forEach(activity => rows += Room.getRow(activity));
-        rows += Room.getActionRow();
+        existingServiceList.forEach(activity => rows += Room.getRow(activity));
+        rows += Room.getActionRow(newServiceList);
         $('#table-overrides tbody').html(rows);
-    }
+    },
+
+    loadService: function (bookedroom) {
+        $.ajax({
+            url: API_URL.GETSERVICES,
+            method: "GET"
+        }).done(function (newServiceList) {
+            console.info('done: serviceList', newServiceList);
+            Room.displayActivities(bookedroom.serviceList, newServiceList);
+        });
+    },
+
+    getActionRow: function(serviceList) {
+        var rows = '';
+        serviceList.forEach(activity => rows += `<option value = "${activity.id}">${activity.serviceName}</option>`);
+        
+        return `<tr>
+            <td>
+                <fieldset>
+                    <select id = "setActivity" onChange="Room.setPrice(this);">
+                        ${rows}
+                    </select>
+                </fieldset>
+            </td>
+            <td id="newPrice"></td>
+            <td><button id="addButton" style="display: none;" type="submit" onClick="Room.add()">Save</button></td>
+            </tr>`;
+    },
+    
+    setPrice: function(selectObject){
+        newActivityId = selectObject.value;
+        var price;
+        console.info('service mata', newServices);
+        newServices.forEach(service => {
+            if(service.id == newActivityId) {
+                price = service.servicePrice;
+            }
+        });
+        console.info('activity', newActivityId)
+        console.info('arrived here', price);
+        $('#newPrice').html(price);
+        document.getElementById("addButton").style.display = "block";
+    },
+
+    getBill: function(){
+        console.info('client', client);
+        console.info('totalPrice', totalPrice);
+
+        var doc = new jsPDF();
+
+        doc.setFont("times");
+        doc.setFontStyle("bolditalic");
+        doc.setFontSize(30);
+        doc.text("Bill for: " + client + " ", 105, 20, null, null, 'center');
+
+        doc.setFont("times");
+        doc.setFontStyle("bold");
+        doc.setFontSize(25);
+        doc.text("You have to pay: " + totalPrice + " lei", 20, 50);
+
+        doc.setFont("courier");
+        doc.setFontSize(20);
+        doc.text("Thank you for choosing our hotel!", 20, 70);
+
+        doc.setFont("courier");
+        doc.setFontSize(20);
+        doc.text("Have a great day!", 20, 80);
+
+        doc.setFont("times");
+        doc.setFontStyle("bolditalic");
+        doc.setFontSize(20);
+        doc.text("Client, ", 20, 100);
+
+        doc.setFont("times");
+        doc.setFontStyle("bolditalic");
+        doc.setFontSize(20);
+        doc.text("Hotel, ", 170, 100);
+
+        doc.save(`${client}.pdf`);
+    },
     
 }
 
 console.info('loading room');
-var bookedroom = '';
+var bookedroom;
+var newServices;
+var newActivityId;
+var client;
+var totalPrice;
 Room.load();
 Room.getBookedRoom();
+//Room.getBill();
